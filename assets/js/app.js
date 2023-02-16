8f5528a09bb3d99fb7349bb2e7eb78d40286814a
Mehwish <mehwishgul715@gmail.com>
2023-02-16 22:18:58 +0500

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,3 +24,15 @@ $(".close-chat").on("click",function(){
     $(".chat-blink").css("display","none");
     $(".click-blink").css("display","block");
 })
+$(".live-chat-btn").on("click",function(){
+  $(".live-chat2").css("opacity","1");
+  $(".live-chat2").css("right","55px");
+  $(this).css("opacity","0");
+  // $(".live-chat2").css(" z-index","100");
+})
+$(".close-chat2").on("click",function(){
+  $(".live-chat2").css("opacity","0");
+  $(".live-chat2").css("right","-400px");
+  $(".live-chat-btn").css("opacity","2")
+  // $(".live-chat2").css(" z-index","0");
+})
